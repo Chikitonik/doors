d9e08fceeba5accd018ffea1f9c3785f441cb2ff
Chikitonik <chikitonik@gmail.com>
2023-02-04 21:19:33 +0300

    init

--- src/App.js
+++ src/App.js
@@ -5,54 +5,7 @@ import canvas from "./assets/images/canvas.png";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import InputAdornment from "@mui/material/InputAdornment";
-
-const PARAMETERS = {
-  A: {
-    name: "Handing",
-    style: { height: 320, width: 30, mt: 50, ml: 17 },
-    meas: "2 ± mm",
-  },
-  B: {
-    name: "Opening Width",
-    style: { height: 30, width: 270, mt: 95, ml: 8 },
-    meas: "2 ± mm",
-  },
-  C: {
-    name: "Opening Height",
-    style: { height: 670, width: 30, mt: 7, ml: 60 },
-    meas: "2 ± mm",
-  },
-  D: {
-    name: "Hinge Size",
-    style: { height: 45, width: 30, mt: 24, ml: 35 },
-    meas: "2 ± mm",
-  },
-  E: {
-    name: "First Hinge",
-    style: { height: 45, width: 30, mt: 7, ml: 43 },
-    meas: "2 ± mm",
-  },
-  F: {
-    name: "Second Hinge",
-    style: { height: 140, width: 30, mt: 8, ml: 44 },
-    meas: "2 ± mm",
-  },
-  G: {
-    name: "Third Hinge (if 4)",
-    style: { height: 360, width: 30, mt: 8, ml: 46 },
-    meas: "2 ± mm",
-  },
-  H: {
-    name: "Fourth Hinge",
-    style: { height: 590, width: 30, mt: 8, ml: 48 },
-    meas: "2 ± mm",
-  },
-  J: {
-    name: "Hinge Type (c/b/p)",
-    style: { height: 190, width: 300, mt: 126, ml: 68 },
-    meas: "c, b, p",
-  },
-};
+import { PARAMETERS } from "./data/PARAMETERS";
 
 function App() {
   const [showBox, setShowBox] = React.useState(false);
@@ -77,17 +30,19 @@ function App() {
         }}
       />
       {Object.entries(PARAMETERS).map(([key, value]) => {
-        return (
-          <Box
-            key={key}
-            sx={{
-              position: "absolute",
-              backgroundColor: "rgba(255, 255, 0, 0.5);",
-              ...value.style,
-              display: showBox[key] ? "block" : "none",
-            }}
-          ></Box>
-        );
+        return Object.entries(value.styles).map(([keyStyle, valueStyle]) => {
+          return (
+            <Box
+              key={key}
+              sx={{
+                position: "absolute",
+                backgroundColor: "rgba(255, 255, 0, 0.4);",
+                ...valueStyle,
+                display: showBox[key] ? "block" : "none",
+              }}
+            ></Box>
+          );
+        });
       })}
       <Paper
         id="parameters"
@@ -96,6 +51,7 @@ function App() {
           zIndex: "tooltip",
           p: 1,
           ml: 69,
+          mt: 0.5,
         }}
       >
         {Object.entries(PARAMETERS).map(([key, value]) => {
@@ -104,8 +60,8 @@ function App() {
               <Typography
                 variant="body1"
                 sx={{
-                  minWidth: 200,
-                  maxWidth: 200,
+                  minWidth: 170,
+                  maxWidth: 170,
                   borderBottom: "1px solid grey",
                 }}
               >{`${key} ${value.name}`}</Typography>
---
+++ src/data/PARAMETERS.js
@@ -0,0 +1,128 @@
+export const PARAMETERS = {
+  A: {
+    name: "Handing",
+    styles: { 1: { height: 320, width: 30, mt: 50, ml: 17 } },
+    meas: "2 ± mm",
+  },
+  B: {
+    name: "Opening Width",
+    styles: {
+      1: { height: 30, width: 270, mt: 95, ml: 8 },
+      2: { height: 30, width: 410, mt: 102, ml: 8 },
+    },
+    meas: "2 ± mm",
+  },
+  C: {
+    name: "Opening Height",
+    styles: { 1: { height: 670, width: 30, mt: 7, ml: 60 } },
+    meas: "2 ± mm",
+  },
+  D: {
+    name: "Hinge Size",
+    styles: { 1: { height: 45, width: 30, mt: 24, ml: 35 } },
+    meas: "2 ± mm",
+  },
+  E: {
+    name: "First Hinge",
+    styles: { 1: { height: 45, width: 30, mt: 7, ml: 43 } },
+    meas: "2 ± mm",
+  },
+  F: {
+    name: "Second Hinge",
+    styles: { 1: { height: 140, width: 30, mt: 8, ml: 44 } },
+    meas: "2 ± mm",
+  },
+  G: {
+    name: "Third Hinge (if 4)",
+    styles: { 1: { height: 360, width: 30, mt: 8, ml: 46 } },
+    meas: "2 ± mm",
+  },
+  H: {
+    name: "Fourth Hinge",
+    styles: { 1: { height: 590, width: 30, mt: 8, ml: 48 } },
+    meas: "2 ± mm",
+  },
+  J: {
+    name: "Hinge Type (c/b/p)",
+    styles: { 1: { height: 190, width: 300, mt: 126, ml: 68 } },
+    meas: "c, b, p",
+  },
+  K: {
+    name: "Treshold",
+    styles: {
+      1: { height: 35, width: 30, mt: 90, ml: 17 },
+      2: { height: 35, width: 30, mt: 90, ml: 60 },
+      3: { height: 180, width: 355, mt: 88, ml: 68 },
+    },
+    meas: "2 ± mm",
+  },
+  L: {
+    name: "Frame Height",
+    styles: {
+      1: { height: 670, width: 30, mt: 7, ml: 53 },
+    },
+    meas: "2 ± mm",
+  },
+  M: {
+    name: "Frame Width",
+    styles: {
+      1: { height: 30, width: 440, mt: 109, ml: 6 },
+    },
+    meas: "2 ± mm",
+  },
+  N: {
+    name: "Frame Thickness",
+    styles: {
+      1: { height: 30, width: 75, mt: 135, ml: 12 },
+      2: { height: 30, width: 75, mt: 134, ml: 40 },
+    },
+    meas: "2 ± mm",
+  },
+  O: {
+    name: "Frame Depth",
+    styles: {
+      1: { height: 170, width: 30, mt: 122, ml: 9 },
+      2: { height: 150, width: 30, mt: 124, ml: 38 },
+      3: { height: 70, width: 30, mt: 106, ml: 1 },
+      4: { height: 30, width: 40, mt: 0, ml: 54 },
+    },
+    meas: "2 ± mm",
+  },
+  P: {
+    name: "Door Rabbet",
+    styles: {
+      1: { height: 60, width: 25, mt: 122, ml: 15 },
+      2: { height: 40, width: 20, mt: 124, ml: 44 },
+    },
+    meas: "2 ± mm",
+  },
+  Q: {
+    name: "Stop Height",
+    styles: {
+      1: { height: 25, width: 35, mt: 131, ml: 17 },
+      2: { height: 25, width: 35, mt: 130, ml: 46 },
+    },
+    meas: "2 ± mm",
+  },
+  X: {
+    name: "Door Thickness",
+    styles: {
+      1: { height: 65, width: 30, mt: 122, ml: 23 },
+      2: { height: 65, width: 30, mt: 121, ml: 52 },
+    },
+    meas: "2 ± mm",
+  },
+  Y: {
+    name: "Lock Types",
+    styles: {},
+    meas: "Y1, Y2, Y3",
+  },
+  Z: {
+    name: "Door Casing",
+    styles: {
+      1: { height: 30, width: 145, mt: 147, ml: 3 },
+      2: { height: 30, width: 135, mt: 147, ml: 33 },
+    },
+    meas: "Z1, Z0",
+  },
+};
